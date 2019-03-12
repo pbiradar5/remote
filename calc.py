9e2ec7ee510f70b7e1448f0af6c214080251357a
pbiradar5 <48424396+pbiradar5@users.noreply.github.com>
2019-03-12 10:11:16 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,13 +1,9 @@
 def add(x,y):
-<<<<<<< HEAD
-    
     x+y
 
 def subtract(x,y):
-=======
-    x+y
+    x-y
     def subtract(x,y):
->>>>>>> 5e51f0605d05e4c70caad6e1e4da0358c838869b
     pass
 def multiply(x,y):
     pass
